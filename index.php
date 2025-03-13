f7e17f0c105596f10c9132c19e54560e824e7d15
Zain Asghar <118249611+zaina59@users.noreply.github.com>
2025-03-13 21:30:09 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+  <link href=statics/css/normalize.css rel=stylesheet type=text/css>
+  <link href=statics/css/webflow.css rel=stylesheet type=text/css>
+  <link href=statics/css/supercook-app.webflow.css rel=stylesheet type=text/css>
+  <script src=../ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js type=text/javascript></script>
+  <script type=text/javascript>WebFont.load({  google: {    families: ["Nunito:300,regular,600,700"]  }});</script>
+  <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','../www.google-analytics.com/analytics.js','ga');
+        
+        var fields = {
+          trackingId: 'UA-35552943-5'
+        };
+      
+        if(document.URL.indexOf('http://') !== 0){
+        
+          fields.clientId = localStorage.getItem('ga:clientId');
+          // disable GA's cookie storage functions
+          fields.storage = 'none';
+          ga('create', fields);
+          ga('set', {
+            checkProtocolTask: null,
+            checkStorageTask: null
+          });
+        
+          ga(function(tracker){
+            localStorage.setItem('ga:clientId', tracker.get('clientId'));
+          });
+        }
+        // if we are in a browser
+        else {    
+          ga('create', fields);
+        }
+
+      //window.dataLayer = window.dataLayer || [];
+      //function gtag(){dataLayer.push(arguments);}
+      //gtag('js', new Date());
+      //gtag('config', 'UA-35552943-5', {'cookieDomain': 'none'});
+      //gtag('set', 'checkProtocolTask', null);
+      </script><meta http-equiv=Content-Security-Policy content="default-src * 'self' data: gap: 'unsafe-inline' 'unsafe-eval';
+          style-src * 'self' 'unsafe-inline' 'unsafe-eval' gap:;
+          script-src * 'self' 'unsafe-inline' 'unsafe-eval' gap:;">
+          <meta charset=utf-8><meta name=format-detection content="telephone=no">
+          <meta name=msapplication-tap-highlight content=no>
+          <meta name=description content="Supercook is a recipe search engine that lets you search by ingredients you have at home. Find thousands of recipes you can make right now with the ingredients you have available at home.  Supercook can help you save hundreds on grocery bills by showing you how to fully use the ingredients you have at home. No more wasted left-overs. Asian,Breads,Breakfast,Cakes,Caribbean,Casseroles,Cookies,Desserts,Dinner,Dips,Drinks,Fish recipes,Grilling & BBQ,Indian & Thai,Italian,Kid Friendly,Meat recipes,Mediterranean,Mexican,Poultry recipes,Quick & Easy,Salad Dressings,Salads,Sandwiches,Sauces,Seafood recipes,Slow Cooker,Soups,Veggie recipes">
+          <meta name=keywords content="dinner ideas,tonight,leftovers,left overs,home,appliances,diet,grocery,supermarket,recipe,search engine,ingredients,pantry,food,kitchen,dinner,dessert,entree,meal">
+          <meta property=og:title content="Supercook: recipe search by ingredients you have at home">
+          <meta property=og:description content="Supercook is a recipe search engine that lets you search by ingredients you have at home. Find thousands of recipes you can make right now with the ingredients you have available at home.">
+          <meta property=fb:app_id content=752086031585448>
+          <meta property=og:image content=/images/002.3cc1abc7.png>
+          <meta name=viewport content="initial-scale=1,width=device-width,height=device-height,viewport-fit=cover">
+          <title>SuperCook - Zero Waste Recipe Generator</title>
+          <script async src="../pagead2.googlesyndication.com/pagead/js/f928c.txt?client=ca-pub-1057233790981454" crossorigin=anonymous></script>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({
+        overlays: {bottom: false,top: true}
+      });</script>
+      <script type=text/javascript>window.pg=window.pg||[];pg.acq=pg.acq||[]; 
+    pg.acq.push(function() {
+    //Google analytics code
+    pg.loadJS("https://www.googletagmanager.com/gtag/js?id=UA-35552943-5");
+    window.dataLayer = window.dataLayer || []; 
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date()); 
+    gtag('config', 'UA-35552943-5');
+    });</script>
+    <script src=../m2d.m2.ai/pghb.supercook.js async></script>
+    <script type=text/javascript src=../cdn.pubguru.com/fb.js></script>
+    <link as=style href=css/app.3e359a2c.css rel=preload>
+    <link as=script href=js/app.390b2007.js rel=preload>
+    <link as=script href=js/runtime.838b9989.js rel=preload>
+    <link as=script href=js/vendor.0d9b3bf6.js rel=preload>
+    <link href=css/1144394c.d2f98ac5.css rel=prefetch>
+    <link href=css/213cde68.f13d641c.css rel=prefetch>
+    <link href=css/28734ebf.dc6d8f8f.css rel=prefetch>
+    <link href=css/2891839a.cd08de9b.css rel=prefetch>
+    <link href=css/2c77ccd6.495f3507.css rel=prefetch>
+    <link href=css/2d0513c6.94433c5d.css rel=prefetch>
+    <link href=css/3b6cfe08.bc9f6b43.css rel=prefetch>
+    <link href=css/40452196.d8107f8d.css rel=prefetch>
+    <link href=css/455dfc86.937f0a2c.css rel=prefetch>
+    <link href=css/48cdfea2.3e402af0.css rel=prefetch>
+    <link href=css/4ad7dd3d.79a79c59.css rel=prefetch>
+    <link href=css/5357e2a0.497fef36.css rel=prefetch>
+    <link href=css/5940a7fa.ba543db3.css rel=prefetch>
+    <link href=css/5b720d5d.eec69d8b.css rel=prefetch>
+    <link href=css/5ce7c3ed.70efef9e.css rel=prefetch>
+    <link href=css/7ea2cedc.50f7146c.css rel=prefetch>
+    <link href=css/8286ef34.38bbc864.css rel=prefetch>
+    <link href=css/a07ad8ba.143c409f.css rel=prefetch>
+    <link href=css/aa1d33de.31b21f9d.css rel=prefetch>
+    <link href=js/1144394c.1bfe2899.js rel=prefetch>
+    <link href=js/213cde68.51b617d0.js rel=prefetch>
+    <link href=js/28734ebf.6a54771f.js rel=prefetch>
+    <link href=js/2891839a.baa0de14.js rel=prefetch>
+    <link href=js/2c77ccd6.3688ebc0.js rel=prefetch>
+    <link href=js/2d0513c6.e49dfb7a.js rel=prefetch>
+    <link href=js/3b6cfe08.e48f6306.js rel=prefetch>
+    <link href=js/40452196.6c882576.js rel=prefetch>
+    <link href=js/455dfc86.66bd1132.js rel=prefetch>
+    <link href=js/48cdfea2.2cf3558d.js rel=prefetch>
+    <link href=js/4ad7dd3d.2938da05.js rel=prefetch>
+    <link href=js/4b4818b8.cd768103.js rel=prefetch>
+    <link href=js/5357e2a0.fd8a7238.js rel=prefetch>
+    <link href=js/5940a7fa.45c66067.js rel=prefetch>
+    <link href=js/5b720d5d.de50b9de.js rel=prefetch>
+    <link href=js/5ce7c3ed.7d1e988b.js rel=prefetch>
+    <link href=js/7ea2cedc.483a4d9f.js rel=prefetch>
+    <link href=js/8286ef34.c4174c7b.js rel=prefetch>
+    <link href=js/a07ad8ba.4d1948d5.js rel=prefetch>
+    <link href=js/aa1d33de.6ea6e068.js rel=prefetch>
+    <link href=css/app.3e359a2c.css rel=stylesheet>
+    </head>
+    <body ondragstart="return false">
+    <noscript>This is your fallback content in case JavaScript fails to load.</noscript>
+    <div id=q-app></div>
+    <script type=text/javascript src=js/app.390b2007.js></script>
+    <script type=text/javascript src=js/runtime.838b9989.js></script>
+    <script type=text/javascript src=js/vendor.0d9b3bf6.js></script>
+    <!-- <img src="C:\xampp\htdocs\supercook\statics\images\male-user-white.png" alt=""> -->
+  </body>
+<!-- Mirrored from www.supercook.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 08 Nov 2022 08:07:00 GMT -->
+</html>
